fc27728bce0668bb51c123706e555fe88e5c87fb
毕小朋 <xiaopeng.bxp@alibaba-inc.com>
2014-03-10 16:31:29 +0800

    test

---
+++ Objective-C/iOSDevelop/一起学习iOS开发一SimpleTable项目/SimpleTable1/SimpleTable/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SimpleTable
+//
+//  Created by bixiaopeng on 14-2-18.
+//  Copyright (c) 2014年 bixiaopeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SimpleTableAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SimpleTableAppDelegate class]));
+    }
+}
---
+++ Objective-C/iOSDevelop/一起学习iOS开发一SimpleTable项目/SimpleTable1/SimpleTableTests/SimpleTableTests.m
@@ -0,0 +1,34 @@
+//
+//  SimpleTableTests.m
+//  SimpleTableTests
+//
+//  Created by bixiaopeng on 14-2-18.
+//  Copyright (c) 2014年 bixiaopeng. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SimpleTableTests : XCTestCase
+
+@end
+
+@implementation SimpleTableTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
